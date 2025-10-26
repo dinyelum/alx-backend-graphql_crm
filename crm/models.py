ef3d97d6a82d578527bdebbf28b134a4f93ce593
Chimdinyelum <chyjohn777@gmail.com>
2025-10-27 00:24:04 +0100

    Edit 4: Week 8: Understanding GraphQL

--- crm/models.py
+++ crm/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)  # Changed to max_length=100
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,7 +18,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)  # Changed to max_length=100
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
     stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     created_at = models.DateTimeField(auto_now_add=True)
