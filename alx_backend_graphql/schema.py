b0df93fa40c860d4bbc5f4de1fdd8d7d43f1f320
Chimdinyelum <chyjohn777@gmail.com>
2025-10-27 00:01:19 +0100

    Edit 2: Week 8: Understanding GraphQL

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,9 @@
 import graphene
 
-class Query(graphene.ObjectType):
+class CRMQuery(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
